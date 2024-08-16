a7622842a0c98ee07f8cb084ed43790b1b30403d
brenda gavilan <brendadguez88@gmail.com>
2024-08-16 11:30:34 +0200

    historial y futuras reservas back

--- Server/routes/reservation.js
+++ Server/routes/reservation.js
@@ -1,8 +1,12 @@
 var express = require("express");
 const reservationController = require("../controllers/reservationController");
+const verifyToken = require("../middlewares/verifyToken");
 var router = express.Router();
 
 router.get('/oneWayTrip', reservationController.oneWayTrip)
 router.get('/returnTrip', reservationController.returnTrip)
+router.get("/historical/:id", verifyToken, reservationController.historical)
+router.get("/nextReservations/:id", verifyToken, reservationController.nextReservations)
+
 
 module.exports = router
