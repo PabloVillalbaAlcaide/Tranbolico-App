97d3f34ed4f4441bbe91e424394674a0dfbcc257
Samuel <samuelestevez@gmail.com>
2024-08-16 18:19:02 +0200

    actualizacion SQL historico

--- Client/src/pages/historical/Historical.jsx
+++ Client/src/pages/historical/Historical.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { Col, Row } from 'react-bootstrap';
 
 export const Historical = () => {
   const [hist, setHist] = useState("historical");
+
   return (
-    <>
-      <Outlet context={{ hist }} />
-    </>
+    <Row>
+      <Col>
+        <div>
+          <button onClick={() => setHist("historical")}>Historial de reservas</button>
+          <button onClick={() => setHist("nextReservations")}>Futuras reservas</button>
+        </div>
+        <Outlet context={{ hist }} />
+      </Col>
+    </Row>
   );
 };
