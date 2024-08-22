06d153db43eba6713500f5f8f3eaa27b994298d9
Samuel <samuelestevez@gmail.com>
2024-08-22 09:12:35 +0200

    inputs uncontroled

--- Client/src/components/DatePicker/DatePicker.jsx
+++ Client/src/components/DatePicker/DatePicker.jsx
@@ -1,24 +1,27 @@
 import { useCallback, useMemo } from "react";
 import { DayPicker } from "react-day-picker";
-import { es } from 'date-fns/locale';
-import { format, isSameDay, parseISO } from 'date-fns';
+import { es } from "date-fns/locale";
+import { format, isSameDay, parseISO } from "date-fns";
 import "react-day-picker/style.css";
 import "./datePicker.scss";
 
 export const TranbolicoDatePicker = ({ date, setDate, planningList }) => {
-  const allowedDates = useMemo(() => 
-    planningList.map((elem) => parseISO(elem.departure_date)), 
+  const allowedDates = useMemo(
+    () => planningList.map((elem) => parseISO(elem.departure_date)),
     [planningList]
   );
 
-  const isAllowedDate = useCallback((date) => {
-    return allowedDates.some((allowedDate) => isSameDay(date, allowedDate));
-  }, [allowedDates]);
+  const isAllowedDate = useCallback(
+    (date) => {
+      return allowedDates.some((allowedDate) => isSameDay(date, allowedDate));
+    },
+    [allowedDates]
+  );
 
   const handleDateChange = (newDate) => {
     if (newDate instanceof Date && !isNaN(newDate)) {
       if (isAllowedDate(newDate)) {
-        const formattedDate = format(newDate, 'yyyy-MM-dd');
+        const formattedDate = format(newDate, "yyyy-MM-dd");
         setDate(formattedDate);
       }
     }
@@ -28,14 +31,6 @@ export const TranbolicoDatePicker = ({ date, setDate, planningList }) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  const CustomNavigation = ({ onPreviousClick, onNextClick, month }) => (
-    <div className="custom-navigation">
-      <button onClick={onPreviousClick}>Anterior</button>
-      <span>{capitalizeFirstLetter(format(month, 'MMMM yyyy', { locale: es }))}</span>
-      <button onClick={onNextClick}>Siguiente</button>
-    </div>
-  );
-
   return (
     <DayPicker
       mode="single"
@@ -43,14 +38,16 @@ export const TranbolicoDatePicker = ({ date, setDate, planningList }) => {
       onSelect={handleDateChange}
       disabled={(day) => !isAllowedDate(day)}
       modifiers={{ allowed: allowedDates }}
-      modifiersClassNames={{ allowed: "allowed-day" }}
-      locale={es}
-      components={{
-        Navigation: CustomNavigation,
+      modifiersClassNames={{
+        weekend: "weekend-day",
+        today: "today-day",
+        allowed: "allowed-day",
       }}
+      locale={es}
       formatters={{
-        formatCaption: (date) => capitalizeFirstLetter(format(date, 'MMMM yyyy', { locale: es })),
-        formatWeekdayName: (date) => format(date, 'EEEEEE', { locale: es }),
+        formatCaption: (date) =>
+          capitalizeFirstLetter(format(date, "MMMM yyyy", { locale: es })),
+        formatWeekdayName: (date) => format(date, "EEEEEE", { locale: es }),
       }}
     />
   );
