0e8be23f6d576ad4a7e58b23bbde58e3fa074442
Samuel <samuelestevez@gmail.com>
2024-08-26 08:35:29 +0200

    Cosillas varias

--- Client/src/pages/Getreservations/GetReservations.jsx
+++ Client/src/pages/Getreservations/GetReservations.jsx
@@ -1,26 +1,37 @@
 import { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
 import { Outlet, useLocation } from "react-router-dom";
 
 export const GetReservations = () => {
   const [reservation, setReservation] = useState();
   const location = useLocation();
   const route = location.state;
-  const [loadingReservation, setLoadingReservation] = useState(true)
+  const [loadingReservation, setLoadingReservation] = useState(true);
 
   useEffect(() => {
     const storedReservation = sessionStorage.getItem("reservation");
-  
+
     if (!reservation && storedReservation) {
       setReservation(JSON.parse(storedReservation));
-    } else if (reservation && storedReservation !== JSON.stringify(reservation)) {
+    } else if (
+      reservation &&
+      storedReservation !== JSON.stringify(reservation)
+    ) {
       sessionStorage.setItem("reservation", JSON.stringify(reservation));
     }
-    setLoadingReservation(false)
+    setLoadingReservation(false);
   }, [reservation]);
 
   return (
     <>
-      <Outlet context={{ reservation, setReservation, route, loadingReservation }} />
+      <Row>
+        <div className="ppal-rutas text-center text-white mt-2">
+          <h2 className="mb-0 py-2">RESERVAS</h2>
+        </div>
+      </Row>
+      <Outlet
+        context={{ reservation, setReservation, route, loadingReservation }}
+      />
     </>
   );
 };
