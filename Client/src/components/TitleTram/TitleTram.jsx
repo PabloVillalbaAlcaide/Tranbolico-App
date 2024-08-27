2c61214ab21cf9b4fa740181d80aa787b36ef727
lucas <lumn033@gmail.com>
2024-08-27 14:31:42 +0200

    anim-scale home btns y addPlanning btns

--- Client/src/components/TitleTram/TitleTram.jsx
+++ Client/src/components/TitleTram/TitleTram.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Row } from 'react-bootstrap'
 import './titleTram.scss'
 
@@ -6,7 +5,7 @@ export const TitleTram = ({children, backgroundColor, color}) => {
   return (
     <Row className="mt-4 banner-faqs d-flex align-items-center justify-content-center" style={{backgroundColor}}>
       <div className="ppal-faqs ps-5 mt-2 flex-grow-1" style={{color}}>
-      <h2 className="py-2 m-0 text-center">{children}</h2>
+      <h2 className="py-2 m-0 text-center" style={{color}}>{children}</h2>
       </div>
     </Row>
   )
